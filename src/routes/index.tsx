e856555aab6c4f36f720a4e7f0f9398efdd88c26
Sivtout666 <maxime.giraudo@free.fr>
2025-03-21 11:39:02 +0100

    ligne en trop inutile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { getDefaultResultOrder } from "dns";
 
 export default component$(() => {
   return <>
