3180f376ed741e8ebc0eae284e530f5860d5f242
Sivtout666 <maxime.giraudo@free.fr>
2025-03-27 14:01:08 +0100

    Sous fichier + debut header

--- src/routes/vie_etudiante/Ville/index.tsx
+++ src/routes/vie_etudiante/Ville/index.tsx
@@ -3,6 +3,6 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
     return <>
-        <div class="h-screen bg-blisen-900"></div>
+        <div class="h-screen bg-amber-700"></div>
     </>
 });
