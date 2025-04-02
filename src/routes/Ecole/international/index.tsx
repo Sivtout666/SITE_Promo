c63fcbe6ddd1c0662bbbfea74488cfa1d68fa261
loshido <loshido@pm.me>
2025-04-02 09:33:03 +0200

    Ajout de la carte interactive

--- src/routes/Ecole/international/index.tsx
+++ src/routes/Ecole/international/index.tsx
@@ -1,8 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 
+import Carte from "~/components/Carte"
+import Partenaire from "~/components/Carte/partenaire"
 
 export default component$(() => {
-    return <>
-        <div class="h-screen bg-amber-700"></div>
-    </>
+    return <section>
+        <div class="h-52 w-full"/>
+        <div class="w-full h-[60vh] relative">
+            <Carte/>
+            <Partenaire/>
+        </div>
+        <div class="h-52 w-full"/>
+    </section>
 });
