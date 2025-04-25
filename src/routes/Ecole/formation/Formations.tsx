540b0cdbbaab855def3884a8e0435953a1d5c152
Tomreichling <tomreichling83@gmail.com>
2025-04-25 11:53:42 +0200

    page foramtion ajout des cartes

--- src/routes/Ecole/formation/Formations.tsx
+++ src/routes/Ecole/formation/Formations.tsx
@@ -1,45 +1,113 @@
 
-export default component$() => <>
-    return <div class="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6">
-     
-      <div class="flex-1 bg-red-700 text-white p-6 rounded-lg shadow-lg">
-        <div class ="flex items-center mb-4">
-          <div class="bg-white text-red-700 text-2xl font-bold rounded-full p-3 mr-4">
-            CIN
+export default component$() => <div class="class=bg-gray-100 p-6">
+
+    <div class="bg-blue-700 w-full h-32">
+      <h1> hello world</h1>
+    </div> 
+
+    <div class="flex flex-col gap-12 max-w-6xl mx-auto">
+
+
+   
+      <div class="relative rounded-xl overflow-hidden shadow-lg">
+        <img src="cin-etudiants.jpg" alt="Prépa Ingénieur" class="w-full h-64 object-cover" />
+        <div class="absolute inset-0 bg-gradient-to-t from-red-800 via-red-700 to-transparent p-6 flex flex-row space-x-32 mt-32 text-white"> 
+          <div class="flex flex-col items-start space-y-2 my-5">
+            <div class="bg-white text-black text-sm font-bold rounded-lg px-3 py-1 shadow">
+              CIN
+            </div>
+            <div class="bg-blue-700 text-white text-sm font-semibold px-3 py-1 rounded-xl shadow">
+              📍 Toulon
+            </div>
           </div>
-          <div class ="bg-blue-600 text-white text-sm font-semibold px-3 py-1 rounded-full">
-            Toulon
+          <div class="flex flex-col">
+            <h2 class="text-lg font-semibold mb-2">Prépa Ingénieur informatique et numérique</h2>
+            <p class="text-sm max-w-md">
+              Formation préparatoire offrant une solide base sur le socle scientifique et en informatique, permettant aux étudiants de poursuivre des études d'ingénieur dans ces domaines.
+            </p>
           </div>
         </div>
-        <h2 class ="text-4xl font-medium mb-4">
-          Prépa Ingénieur informatique et numérique
-        </h2>
-        <p class ="text-xl font-light">
-          Formation préparatoire offrant une solide base sur le socle scientifique 
-          et en informatique, permettant aux étudiants de poursuivre des études 
-          d'ingénieur dans ces domaines.
-        </p>
       </div>
-  
-    
-      <div class ="flex-1 bg-red-700 text-white p-6 rounded-lg shadow-lg">
-        <div class ="flex items-center mb-4">
-          <div class ="bg-white text-red-700 text-2xl font-bold rounded-full p-3 mr-4">
-            Cycle ingé
+
+      <div class="relative rounded-xl overflow-hidden shadow-lg">
+        <img src="biost-etudiants.jpg" alt="BIOST" class="w-full h-64 object-cover " />
+        <div class="absolute inset-0 bg-gradient-to-t from-red-800 via-red-700 to-transparent p-6 flex flex-row space-x-32 mt-32 text-white">
+          <div class="flex flex-col items-start space-y-2 my-5">
+            <div class="bg-white text-black text-sm font-bold rounded-lg px-3 py-1 shadow">
+              BIOST
+            </div>
+            <div class="bg-blue-700 text-white text-sm font-semibold px-3 py-1 rounded-xl shadow">
+              📍 Toulon
+            </div>
           </div>
-          <div class ="bg-blue-600 text-white text-sm font-semibold px-3 py-1 rounded-full">
-            Toulon
+          <div class="flex flex-col">
+            <h2 class="text-lg font-semibold mb-2">Prépa ingénieur biologie, scinces et technologies</h2>
+            <p class="text-sm  max-w-md">
+              Formation pluridisciplinaire à l’interface du vivant, des sciences et des technologies, préparant aux enjeux de la santé, de la biotechnologie et de l’innovation numérique.            </p>
           </div>
         </div>
-        <h2 class ="text-4xl font-medium mb-4">
-          Cycle ingénieur Bac+5
-        </h2>
-        <p class ="text-xl font-light">
-          Formation d’ingénieurs experts et polyvalents, prêts à relever
-           les défis technologiques actuels et futurs grâce à un large 
-           choix de spécialisations.
-        </p>
+      </div>
+
+      <div class="relative rounded-xl overflow-hidden shadow-lg">
+        <img src="cycle-inge.jpg" alt="Cycle Ingénieur" class="w-full h-64 object-cover " />
+        <div class="absolute inset-0 bg-gradient-to-t from-red-800 via-red-700 to-transparent p-6 flex flex-row space-x-32 mt-32 text-white">
+          <div class="flex flex-col items-start space-y-2 my-5">
+            <div class="bg-white text-black text-sm font-bold rounded-lg px-3 py-1 shadow">
+              Cycle ingé
+            </div>
+            <div class="bg-blue-700 text-white text-sm font-semibold px-3 py-1 rounded-xl shadow">
+              📍 Toulon
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <h2 class="text-lg font-semibold mb-2">Cycle ingénieur Bac+5</h2>
+            <p class="text-sm  max-w-md">
+              Formation d'ingénieurs experts et polyvalents, prêts à relever les défis technologiques actuels et futurs grâce à un large choix de spécialisations.
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="relative rounded-xl overflow-hidden shadow-lg">
+        <img src="gaming.jpg" alt="DSNI" class="w-full h-64 object-cover " />
+        <div class="absolute inset-0 bg-gradient-to-t from-red-800 via-red-700 to-transparent p-6 flex flex-row space-x-32 mt-32 text-white">
+          <div class="flex flex-col items-start space-y-2 my-5">
+            <div class="bg-white text-black text-sm font-bold rounded-lg px-3 py-1 shadow">
+              Bach DSNI
+            </div>
+            <div class="bg-blue-700 text-white text-sm font-semibold px-3 py-1 rounded-xl shadow">
+              📍 Marseille
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <h2 class="text-lg font-semibold mb-2">Bachelor Développement de Systèmes Numériques Immersifs</h2>
+            <p class="text-sm  max-w-md">
+              Formation axée sur la création de systèmes interactifs et immersifs, mêlant développement logiciel, technologies XR et innovation numérique pour répondre aux nouveaux usages du virtuel.            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="relative rounded-xl overflow-hidden shadow-lg">
+        <img src="cyber.png" alt="Cyber" class="w-full h-64 object-cover " />
+        <div class="absolute inset-0 bg-gradient-to-t from-red-800 via-red-700 to-transparent p-6 flex flex-row space-x-32 mt-32 text-white">
+          <div class="flex flex-col items-start space-y-2 my-5">
+            <div class="bg-white text-black text-sm font-bold rounded-lg px-3 py-1 shadow">
+              Bach Cyber
+            </div>
+            <div class="bg-blue-700 text-white text-sm font-semibold px-3 py-1 rounded-xl shadow">
+              📍 Toulon
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <h2 class="text-lg font-semibold mb-2">Bachelor Cybersécurité  </h2>
+            <p class="text-sm  max-w-md">
+              Formation spécialisée en cybersécurité offrant une expertise solide en protection des systèmes informatiques, analyse de vulnérabilités et gestion des risques numériques.            </p>
+          </div>
+        </div>
       </div>
 
     </div>
-</>
+
+</div>
