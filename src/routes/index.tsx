3527f7cce057b5723073e9f2682795f888365552
loshido <loshido@pm.me>
2025-04-30 14:52:38 +0200

    Prémisses page d'accueil

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import { animate, scroll } from "motion"
 import Landing from "~/components/Accueil/landing";
 import Clouds from "~/components/Accueil/clouds";
+import Chapeau1 from "~/assets/chapeaux/chapeau1.png?jsx"
+import Livre1 from "~/assets/livres/livre1.png?jsx"
 
 export default component$(() => {
     // eslint-disable-next-line qwik/no-use-visible-task
@@ -23,20 +25,20 @@ export default component$(() => {
             <Clouds/>
         </Landing>
 
-        <section class="h-1/2 bg-lime-600 text-white">
-            <p class="p-8">
-                Qui nous sommes
-            </p>
-
-            {/* 
-                Les ingénieurs formés à l’ISEN disposent d’une solide formation scientifique 
-                et de la maîtrise du numérique.
-            
-                grande école, à but non lucratif et labellisée EESPIG.
-
-                 accompagné tout au long de son parcours de formation, un parcours qui correspond 
-                 autant à ses envies qu’aux besoins des entreprises et de la société.
-            */}
+        <section class="bg-white py-64 px-8 md:p-16 lg:p-32
+            md:grid grid-cols-3 gap-2 items-center justify-center">
+            <div class="col-span-2">
+                <h3 class="text-lg md:text-2xl text-black/25 uppercase font-black">
+                    Qui sommes-nous ?
+                </h3>
+                <div class="text-2xl md:text-4xl/normal font-medium">
+                    Les ingénieurs formés à l’ISEN disposent d’une solide 
+                    formation <span class="font-bold text-isen-500">technique</span> et
+                     <span class="text-blisen-500 font-bold"> humaine </span> 
+                    et d'une maîtrise du <span class="font-bold">numérique</span>.
+                </div>
+            </div>
+            <Chapeau1 class="p-16 sm:p-8 md:p-4 lg:p-0"/>
         </section>
         <section class="h-1/2 bg-blue-600 text-white">
             <p class="p-8">
@@ -51,11 +53,33 @@ export default component$(() => {
                 Bachelor truc..
             */}
         </section>
-        <section class="h-1/2 bg-emerald-600 text-white">
-            <p class="p-8">
-                Pour qui
-            </p>
-
+        <section class="bg-white py-64 px-8 md:p-16 lg:p-32
+            md:grid grid-cols-3 gap-2 items-center justify-center">
+            <Livre1 class="p-16 sm:p-8 md:p-4 lg:p-0"/>
+            <div class="col-span-2">
+                <h3 class="text-lg md:text-2xl text-black/25 uppercase font-black">
+                    Pour qui ?
+                </h3>
+                <div class="text-2xl md:text-4xl/normal font-medium">
+                    Vous pouvez entrer à l'ISEN Méditerranée dès la sortie du Lycée 🏫 ! 
+                    <br/>
+                    <ul class="list-[square] pl-12 uppercase font-semibold
+                        text-isen-500/75 text-2xl my-4">
+                        <li>
+                            BAC Générale ou Technologique
+                        </li>
+                        <li>
+                            Prépa CPGE
+                        </li>
+                        <li>
+                            BUT, BTS, License <span
+                                class="text-isen-500/35 hover:text-isen-500/75 transition-colors">
+                                (Info, Math, Elec ou Réseau)
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             {/* 
                 Entrées BAC+2 et Postbac via parcoursup
             */}
