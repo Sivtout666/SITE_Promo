3c9a4f4343041b01ca7ff3f4f035a9f7c54160a6
loshido <loshido@pm.me>
2025-04-30 14:25:40 +0200

    Blog

--- src/components/Blog/article.tsx
+++ src/components/Blog/article.tsx
@@ -3,15 +3,16 @@ import { Link } from "@builder.io/qwik-city";
 import Amphi from "~/assets/isen/amphi.jpg?jsx"
 import Gala from "~/assets/isen/gala.jpg?jsx"
 import Sud from "~/assets/isen/sud.jpg?jsx"
-
+import Toulon from "~/assets/toulon.jpg?jsx"
 
 type Props = {
     titre: string,
     description: string,
-    image: 'amphi' | 'gala'| 'sud'
+    href?: string,
+    image: 'amphi' | 'gala'| 'sud' | 'toulon'
 } & PropsOf<'article'>
-export default component$(({ titre, description, image, ...props }: Props) => {
-    return <Link>
+export default component$(({ titre, description, image, href, ...props }: Props) => {
+    return <Link href={href}>
         <article {...props} class={["w-full h-full rounded cursor-pointer p-4",
                 "flex flex-col gap-2 bg-white text-white relative group", props.class]}>
             <h2 class="text-2xl font-semibold z-20">
@@ -20,7 +21,7 @@ export default component$(({ titre, description, image, ...props }: Props) => {
             <p class="z-20">
                 { description }
             </p>
-            <div class="z-10 bg-gradient-to-br from-black/75 via-black/75 via-20% to-black/25 group-hover:to-black/0
+            <div class="z-10 bg-gradient-to-br from-black/50 via-black/75 via-20% to-black/25 group-hover:to-black/0
                 w-full h-full absolute top-0 left-0 transition-colors duration-500 rounded"/>
 
             {
@@ -30,6 +31,10 @@ export default component$(({ titre, description, image, ...props }: Props) => {
             {
                 image === 'gala' && 
                 <Gala class="z-0 absolute top-0 left-0 w-full h-full object-cover rounded"/>
+            }
+            {
+                image === 'toulon' && 
+                <Toulon class="z-0 absolute top-0 left-0 w-full h-full object-cover rounded"/>
             }
             {
                 image === 'sud' && 
